7affb0e33556ad198b8f50e177ae493d81125950
gohackathon2017 <gohackathon6@gmail.com>
2024-11-13 22:44:17 +0530

    Update apps.py

--- app1/apps.py
+++ app1/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+from .services import load_models
 
 class App1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app1'
+
+    def ready(self):
+        # Load saved models when the application starts
+        load_models()
